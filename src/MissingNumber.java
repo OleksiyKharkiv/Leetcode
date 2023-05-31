a9582514ead1436ee7d98d91c3665ff6be1b0087
Oleksiy Khudoshyn <hudoshin7605@gmail.com>
2023-05-31 21:20:13 +0200

    LeetCode. Training.1.2

---
+++ src/MissingNumber.java
@@ -0,0 +1,19 @@
+public class MissingNumber {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+
+        // Вычисляем ожидаемую сумму чисел в диапазоне [0, n]
+        int expectedSum = (n * (n + 1)) / 2;
+
+        // Вычисляем фактическую сумму чисел в массиве
+        int actualSum = 0;
+        for (int num : nums) {
+            actualSum += num;
+        }
+
+        // Пропущенное число равно разности между ожидаемой суммой и фактической суммой
+        int missingNumber = expectedSum - actualSum;
+
+        return missingNumber;
+    }
+}
