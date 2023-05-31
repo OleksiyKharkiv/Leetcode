705e7c1cf0cb2ba73f37037000f0d6a416a99358
Oleksiy Khudoshyn <hudoshin7605@gmail.com>
2023-05-31 22:27:04 +0200

    LeetCode. Training.1.3

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         MiddleOfLinkedList mid = new MiddleOfLinkedList();
-
         MiddleOfLinkedList.ListNode head = new MiddleOfLinkedList.ListNode(1, null);
         head.next = new MiddleOfLinkedList.ListNode(2, null);
         head.next.next = new MiddleOfLinkedList.ListNode(3, null);
@@ -13,12 +11,17 @@ public class Main {
 
         MaximumWealth maximumWealth = new MaximumWealth();
         int[][] accounts = {
-                {1,2,3,4},
-                {2,3,4,12},
-                {3,4,5,6}
+                {1, 2, 3, 4},
+                {2, 3, 4, 12},
+                {3, 4, 5, 6}
         };
         int wealth = maximumWealth.maximumWealth(accounts);
         System.out.println("Wealth is:  " + wealth);
 
+        TwoSumHashMap twoSumHashMap = new TwoSumHashMap();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int res[] = twoSumHashMap.twoSum(nums, target);
+        System.out.println("Result: [" + res[0] + "," + res[1] + "]");
     }
 }
