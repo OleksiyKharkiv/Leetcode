0df32ba3f0d2d1068763a4aba55294bff1980601
Oleksiy Khudoshyn <hudoshin7605@gmail.com>
2023-06-05 21:17:22 +0200

    LeetCode. Training.1.6

---
+++ src/QuickSort.java
@@ -0,0 +1,28 @@
+public class QuickSort {
+    public static void quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(array, low, high);
+            quickSort(array, 0, pivotIndex - 1);
+            quickSort(array, pivotIndex + 1, high);
+        }
+    }
+
+    public static int partition(int[] array, int low, int high) {
+        int pivot = high;
+        int i = low - 1;
+        for (int j = low; j < high; i++) {
+            if (array[j] < pivot) {
+                i++;
+                swap(array, i, j);
+            }
+        }
+        swap(array, i + 1, high);
+        return i + 1;
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[i] = array[i];
+    }
+}
