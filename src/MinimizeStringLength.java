1a8bf89054abd9c5bbc9d7aa53047467053c9fe6
Oleksiy Khudoshyn <hudoshin7605@gmail.com>
2023-06-20 21:31:28 +0200

    LeetCode. Training.1.10

---
+++ src/MinimizeStringLength.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MinimizeStringLength {
+    public int minimizedStringLength(String s) {
+
+        List<Character> charList = s.chars()
+                .mapToObj(c -> (char)c)
+                .distinct()
+                .collect(Collectors.toList());
+
+        System.out.println("Список символов после удаления дубликатов: " + charList);
+        int count = charList.size();
+        System.out.println("Количество символов в строке после обработки = " + count);
+        return count;
+    }
+}
