b86f9c0d9e55bb493a67d243e6bc4e1aff3ba16a
Oleksiy Khudoshyn <hudoshin7605@gmail.com>
2023-07-04 12:04:57 +0200

    LeetCode. Training.1.14

---
+++ src/ClimbingStairs.java
@@ -0,0 +1,17 @@
+public class ClimbingStairs {
+    public int climbStairs(int n) {
+        int res = 0;
+        if (n <= 2) {
+            return n;
+        }
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+        return dp[n];
+    }
+}
