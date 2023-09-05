3ccbc5334bcbade18b9dd0004075725f3adb0089
Oleksiy Khudoshyn <hudoshin7605@gmail.com>
2023-09-05 22:06:09 +0200

    LeetCode. Training.1.34

---
+++ src/OleksiyKhudoshynJavaDeveloper.java
@@ -0,0 +1,8 @@
+public class OleksiyKhudoshynJavaDeveloper {
+    public static void main(String[] args) {
+        String occupation = "Java Backend Developer";
+        String mobile = "Mob. +4915204799383";
+        String email = "hudoshin7605@gmail.com";
+        String gitHub = "GitHub: https://github.com/OleksiyKharkiv";
+    }
+}
