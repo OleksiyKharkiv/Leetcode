6e133886d2baaffb1344fa84585e26c0095a8336
Oleksiy Khudoshyn <hudoshin7605@gmail.com>
2023-09-28 12:19:33 +0200

    LeetCode. Training.1.43

---
+++ src/PascalsTriangle.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalsTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        if (numRows < 1) {
+            return null;
+        }
+        List<List<Integer>> generate = new ArrayList<>();
+        if (numRows == 1) {
+            List<Integer> list0 = new ArrayList<>();
+            list0.add(1);
+            generate.add(list0);
+            return generate;
+        } else {
+
+            List<Integer> list0 = new ArrayList<>();
+            list0.add(1);
+            generate.add(list0);
+            for (int i = 1; i < numRows; i++) {
+                List<Integer> list = new ArrayList<>();
+                for (int j = 0; j <= i; j++) {
+                    if (j == 0 || j == i) {
+                        list.add(1);
+                    } else {
+                        list.add(j, generate.get(i - 1).get(j - 1) + generate.get(i - 1).get(j));
+                    }
+                }
+                generate.add(list);
+            }
+            return generate;
+        }
+    }
+}
