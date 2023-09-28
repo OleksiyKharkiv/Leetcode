58c96eb34f952dc246dae6027328411f23c19327
Oleksiy Khudoshyn <hudoshin7605@gmail.com>
2023-09-28 09:54:59 +0200

    LeetCode. Training.1.42

---
+++ src/PathSum.java
@@ -0,0 +1,29 @@
+public class PathSum {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        targetSum -= root.val;
+        if (root.left == null && root.right == null) {
+            return targetSum == 0;
+        }
+        boolean leftPath = hasPathSum(root.left, targetSum);
+        boolean rightPath = hasPathSum(root.right, targetSum);
+        return leftPath || rightPath;
+    }
+}
