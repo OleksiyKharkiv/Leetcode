c7229ea76dfaa5baf8bd58d5da98a436f83f4d66
Oleksiy Khudoshyn <hudoshin7605@gmail.com>
2023-12-30 20:06:44 +0100

    LeetCode. Training.1.49

--- src/BestTimeToBuyAndSellStock.java
+++ src/BestTimeToBuyAndSellStock.java
@@ -1,26 +1,21 @@
 public class BestTimeToBuyAndSellStock {
     public int maxProfit(int[] prices) {
+        if (prices.length < 1){
+            return 0;
+        }
+        int profit = 0;
         int maxProfit = 0;
-        if (prices.length < 2){
-            return maxProfit;
-        }
-        if (prices.length == 2 && prices[1] > prices[0]){
-            maxProfit = prices[1] - prices[0];
-            return maxProfit;
-        }
-        if (prices.length == 2 && prices[1] <= prices[0]){
-            maxProfit = 0;
-            return maxProfit;
-        }
-        else {
-            for (int i = 0 ; i < prices.length - 1; i++){
-                for (int j = i+1; j < prices.length; j++ ){
-                    if ((prices[j] - prices[i]) > maxProfit){
-                        maxProfit = prices[j] - prices[i];
-                    }
-                }
+        int minPrice = prices[0];
+        for (int i = 1 ; i < prices.length; i++){
+            profit = prices[i] - minPrice;
+            if (profit > maxProfit){
+                maxProfit = profit;
             }
-            return maxProfit;
+            if (prices[i] < minPrice){
+                minPrice = prices[i];
+            }
         }
+        return maxProfit;
+
     }
 }
