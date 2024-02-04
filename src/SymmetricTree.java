015cdaf24617b218573b7e18c5a397ea14f1276a
Oleksiy Khudoshyn <hudoshin7605@gmail.com>
2024-02-04 20:02:01 +0100

    LeetCode. Training.1.52

--- src/SymmetricTree.java
+++ src/SymmetricTree.java
@@ -1,5 +1,5 @@
 public class SymmetricTree {
-//**
+    //**
 //        * Definition for a binary tree node.
 //        * public class TreeNode {
 // *     int val;
@@ -15,18 +15,19 @@ public class SymmetricTree {
 // * }
 // */
     public boolean isSymmetric(TreeNode root) {
-        if (root == null){
+        if (root == null) {
             return true;
         }
         return isMirror(root.left, root.right);
     }
-    private boolean isMirror(TreeNode left, TreeNode right){
-        if (left == null && right == null ){
+
+    private boolean isMirror(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
             return true;
         }
-        if (left == null || right == null){
+        if (left == null || right == null) {
             return false;
         }
-        return (left.val == right.val)&&isMirror(left.left, right.right)&&isMirror(left.right, right.left);
+        return (left.val == right.val) && isMirror(left.left, right.right) && isMirror(left.right, right.left);
     }
 }
