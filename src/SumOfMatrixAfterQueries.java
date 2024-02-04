b31bde97628429adaaab2ae6d7641723dd276a85
Oleksiy Khudoshyn <hudoshin7605@gmail.com>
2024-02-04 20:01:04 +0100

    LeetCode. Training.1.52

--- src/SumOfMatrixAfterQueries.java
+++ src/SumOfMatrixAfterQueries.java
@@ -4,26 +4,25 @@ public class SumOfMatrixAfterQueries {
         int[][] matrix = new int[n][n];
         long sum = 0;
 
-        for(int q=0; q < queries.length; q++){
+        for (int q = 0; q < queries.length; q++) {
             int type = queries[q][0];
             int index = queries[q][1];
             int val = queries[q][2];
-            if (type==0){
-                for (int i=0; i < n; i++ ){
+            if (type == 0) {
+                for (int i = 0; i < n; i++) {
                     matrix[index][i] = val;
                 }
-            }
-            else if (type==1){
-                for (int i=0; i < n; i++ ){
+            } else if (type == 1) {
+                for (int i = 0; i < n; i++) {
                     matrix[i][index] = val;
                 }
 
             }
         }
 
-        for (int[] row: matrix){
-            for (int element: row){
-                sum+=element;
+        for (int[] row : matrix) {
+            for (int element : row) {
+                sum += element;
             }
         }
         System.out.println("Matrix sum is: " + sum);
