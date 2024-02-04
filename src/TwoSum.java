b8afa0e2e510487b1dc46e138d05b9de27446ab4
Oleksiy Khudoshyn <hudoshin7605@gmail.com>
2024-02-04 20:02:43 +0100

    LeetCode. Training.1.52

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -2,9 +2,9 @@ public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         int[] res = new int[2];
 
-        for (int i=0; i<nums.length; i++){
-            for (int j=1; j<nums.length; j++){
-                if (nums[i] + nums[j] == target && i!=j){
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target && i != j) {
                     res[0] = i;
                     res[1] = j;
                     return res;
