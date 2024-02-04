a322a79ba794e9ed2c39106dc2dc85fd2843870c
Oleksiy Khudoshyn <hudoshin7605@gmail.com>
2024-02-04 20:03:46 +0100

    LeetCode. Training.1.52

--- src/ValidPalindrome.java
+++ src/ValidPalindrome.java
@@ -1,18 +1,16 @@
 public class ValidPalindrome {
-    public boolean isPalindrom(String s){
+    public boolean isPalindrom(String s) {
         if (s == null) {
             return true;
-        }
-        else{
+        } else {
             String sMod = s.replaceAll("^a-zA-Z0-9", "");
             sMod = sMod.toLowerCase();
-            char [] charArr = sMod.toCharArray();
+            char[] charArr = sMod.toCharArray();
             int i = 0;
-            while (i <= sMod.length()/2-1){
-                if (charArr[i] ==charArr[charArr.length-1-i]){
+            while (i <= sMod.length() / 2 - 1) {
+                if (charArr[i] == charArr[charArr.length - 1 - i]) {
                     i++;
-                }
-                else return false;
+                } else return false;
             }
             return true;
         }
