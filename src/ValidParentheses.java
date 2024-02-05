231b5e76a383a26c5dc7fd025ab801913c6cbc78
Oleksiy Khudoshyn <hudoshin7605@gmail.com>
2024-02-05 23:56:41 +0100

    LeetCode. Training.1.52

--- src/ValidParentheses.java
+++ src/ValidParentheses.java
@@ -6,16 +6,15 @@ public class ValidParentheses {
             return false;
         }
         Stack<Character> stack = new Stack<>();
-        for (char c: s.toCharArray()){
-            if ( c == '(' || c == '[' || c == '{'){
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
                 stack.push(c);
-            }
-            else {
-                if (stack.isEmpty()){
+            } else {
+                if (stack.isEmpty()) {
                     return false;
                 }
                 char top = stack.pop();
-                if (( c == ')'&& top !='(' ) ||  ( c == ']'&& top !='[' ) || ( c == '}'&& top !='{' )  ){
+                if ((c == ')' && top != '(') || (c == ']' && top != '[') || (c == '}' && top != '{')) {
                     return false;
                 }
             }
