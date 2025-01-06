b043712dcb1075973cb05806d9db1e7670cfea6f
OleksiyKhudoshyn <hudoshin7605@gmail.com>
2025-01-06 18:46:41 +0100

    LeetCode. Training.1.69

--- src/RemoveNthFromEnd.java
+++ src/RemoveNthFromEnd.java
@@ -4,20 +4,23 @@ public class RemoveNthFromEnd {
         dummy.next = head;
         ListNode first = dummy;
         ListNode second = dummy;
-        for (int i = 0; i <= n; i++){
+        for (int i = 0; i <= n; i++) {
             first = first.next;
         }
-        while (first != null){
+        while (first != null) {
             first = first.next;
             second = second.next;
         }
         second.next = second.next.next;
-        return dummy;
+        return dummy.next;
     }
+
     class ListNode {
         int val;
         ListNode next;
-        ListNode(int val) { this.val = val; }
-    }
 
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
 }
