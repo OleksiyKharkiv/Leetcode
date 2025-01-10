a9e5d6e5dea9a16eeb082c94ec70ec2d4701e13b
OleksiyKhudoshyn <hudoshin7605@gmail.com>
2025-01-10 17:17:36 +0100

    LeetCode. Training.1.71

---
+++ src/SwapNodePairs.java
@@ -0,0 +1,23 @@
+public class SwapNodePairs {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null){
+            return head;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        ListNode curr = head;
+        while (curr != null && curr.next != null){
+            ListNode second = curr.next;
+            ListNode nextPair = curr.next.next;
+            second.next = curr;
+            curr.next = nextPair;
+            prev.next = second;
+
+            prev = curr;
+            curr = nextPair;
+
+        }
+    }
+
+}
