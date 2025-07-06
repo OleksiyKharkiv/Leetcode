43110c60268a0ac549c82c9cdb0010dc8bd752d5
OleksiyKharkiv <hudoshin7605@gmail.com>
2025-07-06 10:35:40 +0200

    Training 1.83

---
+++ src/PowerOfFour.java
@@ -0,0 +1,8 @@
+public class PowerOfFour {
+    public boolean isPowerOfFour(int n) {
+        if (n == 1) return true;
+        if (n == 0 || n%4 != 0) return false;
+
+        return isPowerOfFour(n / 4);
+    }
+}
