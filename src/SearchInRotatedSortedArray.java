f2f86b15c0966eba202c600991af6cc8a50ff46d
OleksiyKharkiv <hudoshin7605@gmail.com>
2025-07-15 20:54:02 +0200

    Training 1.86

--- src/SearchInRotatedSortedArray.java
+++ src/SearchInRotatedSortedArray.java
@@ -1,31 +1,29 @@
 public class SearchInRotatedSortedArray {
     public int search(int[] nums, int target) {
-        if (nums.length == 1 && nums[0] == target){
+        if (nums.length == 1 && nums[0] == target) {
             return 0;
         }
 
         int left = 0;
         int right = nums.length - 1;
-        while (left <= right){
-            int mid = left + (right-left)/2;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
                 return mid;
             }
 
-            if (nums[left] <= nums[mid]){
-                if (nums[left] <= target && target < nums[mid]){
-                    right = mid -1;
-                }
-                else {
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
                     left = mid + 1;
                 }
             } else {
                 if (nums[mid] < nums[right]) {
-                    if (nums[mid+1] <= target && target <= nums[right]){
+                    if (nums[mid + 1] <= target && target <= nums[right]) {
                         left = mid + 1;
-                    }
-                    else {
-                        right = mid -1;
+                    } else {
+                        right = mid - 1;
                     }
                 }
             }
