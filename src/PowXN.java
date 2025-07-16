5492aa9265ad69f142f92910c2bacc152bb93130
OleksiyKharkiv <hudoshin7605@gmail.com>
2025-07-16 17:55:29 +0200

    Training 1.87

--- src/PowXN.java
+++ src/PowXN.java
@@ -1,17 +1,18 @@
 public class PowXN {
-    public double myPow(double x, int n){
+    public double myPow(double x, int n) {
         long N = n;
         if (n == 0) return 1;
         if (n < 0) {
-            x = 1/x;
+            x = 1 / x;
             N = -N;
         }
-        return fastPow (x, N);
+        return fastPow(x, N);
     }
-    private double fastPow (double x, long n){
+
+    private double fastPow(double x, long n) {
         if (n == 0) return 1;
-        double half = fastPow(x, n/2);
-        if (n%2 == 0) return half*half;
-        else return half*half*x;
+        double half = fastPow(x, n / 2);
+        if (n % 2 == 0) return half * half;
+        else return half * half * x;
     }
 }
