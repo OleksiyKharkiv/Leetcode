b7481a0e49649a972775eda0e6c24d7bf1de8093
OleksiyKharkiv <hudoshin7605@gmail.com>
2025-08-02 19:11:34 +0200

    Training 1.90

--- src/CountAndSay.java
+++ src/CountAndSay.java
@@ -1,2 +1,20 @@
 public class CountAndSay {
+    public String countAndSay(int n){
+        if (n ==1) return "1";
+        String prev = countAndSay(n-1);
+        StringBuilder res = new StringBuilder();
+        int count = 0;
+        char currChar = prev.charAt(0);
+        for (int i = 0; i < prev.length(); i++){
+            if (prev.charAt(i) == currChar){
+                count++;
+            } else {
+                res.append(count).append(currChar);
+                currChar = prev.charAt(i);
+                count = 1;
+            }
+        }
+        res. append(count).append(currChar);
+        return res.toString();
+    }
 }
